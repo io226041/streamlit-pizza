b929c995391e8bde39427315cbe819313d5116f6
iuri <iuri.olari@elara-gmbh.de>
2023-11-25 11:36:23 +0100

    sidebar extracted to function, cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,43 +17,52 @@ def create_model(csv_file):
 
 def predict_price(pizza_data_record, model):
     df = pd.DataFrame([pizza_data_record])
-    print(df)
-    print(df.transpose())
     predicted_price = model.predict(df)
     return predicted_price.values[0]
 
 
-pizza_data_record = {
-    'Intercept': 2,                     #
-    'Topping 1': 0,                     # 0: no, 1: yes
-    'Topping 2': 1,                     # 0: no, 1: yes
-    'Topping 3': 0,                     # 0: no, 1: yes
-    'Size': 1,                          # 0: Small, 1: Large, 2: Big
-    'Extras Sauce': 1,                  # 0: no, 1: yes
-    'Extra Cheese': 0,                  # 0: no, 1: yes
-    'Distance to City Center (km)': 3,  # 1,3,5,10 km
-    'Restaurant': 0,                    # 0: Take-Away, 1: Dine-In
-    'Rating': 4                         # 1,2,3,4,5,6 Stars
-}
+def sidebar():
+    st.sidebar.write("Toppings")
+    topping1 = st.sidebar.selectbox(label=f"#1 - sauce", options=(["no sauce", "Tomato Sauce", "Pesto", "Alfredo"]), label_visibility="collapsed")
+    topping2 = st.sidebar.selectbox(label=f"#2 - vegetable", options=(["no vegetable", "Pepperoni", "Mushrooms", "Onions", "Tomatoes"]), label_visibility="collapsed")
+    topping3 = st.sidebar.selectbox(label=f"#3 - meat", options=(["no meat", "Sausage", "Bacon", "Gyros"]), label_visibility="collapsed")
+    size_labels = {0: "Small", 1: "Large", 2: "Big"}
+    pizza_size = st.sidebar.select_slider("Size", [0, 1, 2], 1, format_func=lambda x: size_labels[x])
+    extra_sauce = st.sidebar.toggle("Extra Sauce")
+    extra_cheese = st.sidebar.toggle("Extra Cheese")
+    distance = st.sidebar.select_slider("Distance from City Center", [1, 3, 5, 10], 3, format_func=lambda x: f"{x}km")
+    location_labels = {0: "Take-Away", 1: "Dine-In"}
+    location_choice = st.sidebar.radio("Location", [0, 1], format_func=lambda x: location_labels[x])
+    rating = st.sidebar.select_slider("Restaurant Raiting", [1, 2, 3, 4, 5, 6], 4, format_func=lambda x: f"{x * '★'}")
+
+    pizza_data_record = {
+        'Intercept': 2,                     #
+        'Topping 1': int(not topping1.startswith("no")),  # 0: no, 1: yes
+        'Topping 2': int(not topping2.startswith("no")),  # 0: no, 1: yes
+        'Topping 3': int(not topping3.startswith("no")),  # 0: no, 1: yes
+        'Size': pizza_size,                               # 0: Small, 1: Large, 2: Big
+        'Extras Sauce': int(extra_sauce),                 # 0: no, 1: yes
+        'Extra Cheese': int(extra_cheese),                # 0: no, 1: yes
+        'Distance to City Center (km)': distance,         # 1,3,5,10 km
+        'Restaurant': location_choice,                    # 0: Take-Away, 1: Dine-In
+        'Rating': rating                                  # 1,2,3,4,5,6 Stars
+    }
+    return pizza_data_record
+
+
+###################
+#    App Start    #
+###################
+
+st.title("Pizza Price Predictor")
 
 # create model
 model = create_model(csv_file="pizza_dataset_relative_price.csv")
-st.write(model.summary())
+
+# load sidebar and get input data
+pizza_data_record = sidebar()
 
 # predict price
 price = predict_price(pizza_data_record, model)
-st.write(f"price: {price} €")
-
-st.sidebar.write("Toppings")
-topping1 = st.sidebar.selectbox(label=f"#1 - sauce", options=(["no sauce", "Tomato Sauce", "Pesto", "Alfredo"]), label_visibility="collapsed")
-topping2 = st.sidebar.selectbox(label=f"#2 - vegetable", options=(["no vegetable", "Pepperoni", "Mushrooms", "Onions", "Tomatoes"]), label_visibility="collapsed")
-topping3 = st.sidebar.selectbox(label=f"#3 - meat", options=(["no meat", "Sausage", "Bacon", "Gyros"]), label_visibility="collapsed")
-size_labels = {0: "Small", 1: "Large", 2: "Big"}
-pizza_size = st.sidebar.select_slider("Size", [0, 1, 2], 1, format_func=lambda x: size_labels[x])
-extra_sauce = st.sidebar.toggle("Extra Sauce")
-extra_cheese = st.sidebar.toggle("Extra Cheese")
-distance = st.sidebar.select_slider("Distance from City Center", [1, 3, 5, 10], 3, format_func=lambda x: f"{x}km")
-location_labels = {0: "Take-Away", 1: "Dine-In"}
-location_choice = st.sidebar.radio("Location", [0, 1], format_func=lambda x: location_labels[x])
-rating = st.sidebar.select_slider("Restaurant Raiting", [1, 2, 3, 4, 5, 6], 4, format_func=lambda x: f"{x * '★'}")
+st.header(f"price: {price:.2f} €")
 
